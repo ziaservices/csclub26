53aae48b456c0cc154e4062350187d2ceb7be342
csclub26 <loggerlightshadow@gmail.com>
2025-10-14 09:36:22 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: '/csclub26/', // 👈 very important
   server: {
     host: "0.0.0.0",          // allow external access (LAN, No-IP)
     port: 80,                 // run on port 80
     allowedHosts: [
-      "csclubtest.ddns.net",  // ✅ your No-IP domain
+      "csclubtest.ddns.net",
+      "csclub26.github.io",  // ✅ your No-IP domain
       "localhost",
       "127.0.0.1",
     ],
